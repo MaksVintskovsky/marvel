d6ed742eed69c10409879eab8a6c5bdc1266e0b7
Maks <intadmirmaks@gmail.com>
2025-06-06 21:20:31 +0200

    Charlist

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -25,7 +25,7 @@ class MarvelService {
     // }
 
     getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
@@ -36,6 +36,7 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
