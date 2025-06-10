718dfdcc55dada06857dc97db152d306845ccea7
Maks <intadmirmaks@gmail.com>
2025-06-10 08:49:17 +0200

    final

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -111,5 +112,7 @@ const View = ({char}) => {
         </>
     )
 }
-
+CharInfo.propTypes = {
+  charId: PropTypes.number
+};
 export default CharInfo;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,7 +4,7 @@ class MarvelService {
 
     _apiBase = 'https://marvel-server-zeta.vercel.app/';
     _apiKey = 'apikey=d4eecb0c66dedbfae4eab45d312fc1df';
-
+    _baseOffset = 0;
     getResource = async (url) => {
         let res = await fetch(url);
     
@@ -24,8 +24,9 @@ class MarvelService {
     //     return await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);;
     // }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+
         return res.data.results.map(this._transformCharacter);
     }
 
